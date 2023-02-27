f7fb510b73d72103ffd1f2844b2c5f11b386edc5
SUNDUS-THE-CODER <sundusahmed648@gmail.com>
2023-02-27 15:39:15 +0000

    add function for handling form submittion

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function Form(props) {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const [submittedData, setSubmittedData] = useState([]);
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,12 +13,26 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    setSubmittedData([...submittedData, { firstName: firstName, lastName: lastName }]);
+    setFirstName('');
+    setLastName('');
+  };
+
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
+      </form>
+      {submittedData.map((key, index) => (
+        <div key={index}>
+          {key.firstName} {key.lastName}
+        </div>
+      ))}
+    </>
   );
 }
 
